113517b7cb559a445e81804aa3b3b447a366d25d
jEllerGRC <eller.jared@student.greenriver.edu>
2024-10-11 22:18:16 -0700

    Attempted to implement some routing, things still seem to be busted though. I have to wake up for work in 6 hours, I need some sleep.

--- home/index.js
+++ home/index.js
@@ -3,15 +3,19 @@ import http from "http";
 import fs from "fs";
 
 //i hope i'm doing this right...
-fs.readFile("./public/home.html", (err, page) => {
-    if (err) {
-        console.log("An error occurred.");
-    }
-    else {
-        const server = http.createServer((req, res) => {
-            res.writeHeader(200, {"Content-Type": "text/html"});
-            res.write(page);
-            response.end();
-        }).listen(3030);
-    }
+const app = express();
+
+//static webpage
+app.use(express.static("./public"));
+
+//routing
+app.get("/home", (req, res) => {
+    fs.readFile("./public/home.html", (err, page) => {
+        if (err) {
+            console.log("An error occurred.");
+        }
+        
+        res.status(200).send("Hello world!");
+        res.end();
+    });
 });
