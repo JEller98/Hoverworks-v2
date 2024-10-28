31e83a8eed6fd1a26f0ec583367b28f61d2b2921
jEllerGRC <eller.jared@student.greenriver.edu>
2024-10-28 16:42:30 -0700

    Added in the bare minimum for routes; each one will send a 200 status code if successfully connected.

--- routers/router.js
+++ routers/router.js
@@ -1,5 +1,4 @@
 import express from "express";
-import fs from "fs";
 
 const router = express.Router();
 
@@ -7,6 +6,44 @@ const router = express.Router();
 router.get("/", (req, res) => {
     //return something with the response, or else it'll freeze
     res.status(200).send("Hello, world!");
+});
+
+//products routes
+router.post("/products", (req, res) =>{
+    //C for create
+    res.status(200).send("POST products");
+});
+
+router.get("/products", (req, res) => {
+    //R for read
+    res.status(200).send("GET products");
+});
+
+router.put("/products", (req, res) => {
+    //U for update
+    res.status(200).send("PUT products");
+});
+
+router.delete("/products", (req, res) => {
+    //D for delete
+    res.status(200).send("DELETE products")
+});
+
+//orders routes
+router.post("/orders", (req, res) => {
+    res.status(200).send("POST orders");
+});
+
+router.get("/orders", (req, res) => {
+    res.status(200).send("GET orders");
+});
+
+router.put("/orders", (req, res) => {
+    res.status(200).send("PUT orders");
+});
+
+router.delete("/orders", (req, res) => {
+    res.status(200).send("DELETE orders");
 });
 
 export default router;
