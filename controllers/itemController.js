87da16bef3572b9ee5dc27722987a77a327464dd
jEllerGRC <eller.jared@student.greenriver.edu>
2024-11-01 14:43:23 -0700

    Added functionality for NULLable properties and implemented checks to ensure that non-NULLable properties aren't NULL.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -40,26 +40,28 @@ const getItemByID = async (req, res) => {
     }
 }
 
+//this function's almost running off my screen. definitely need to write some utility functions to automate some of the checks
 const addItem = async (req, res) => {
-    //get the non-NULLable fields out of the body and send them off
-    const {prodType, prodPrice, prodName} = req.body;
+    //get the properties out of the request body
+    const {prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize} = req.body;
 
-    //ensuring the user enters a number for the price
-    if(isNaN (prodPrice)) {
+    //checking for NULLs in non-NULLable columns
+    if(!prodType || !prodPrice || !prodName) {
         res.status(400).json({
-            message: `prodPrice property must be numeric.`
-        }).send();
+            message: `prodType, prodPrice and prodName cannot be NULL.`
+        });
+        res.send();
     }
     else {
-        const result = await db.addItem(prodType, prodPrice, prodName);
-
-        //if we don't get any response back from the DB...
-        if (result === null) {
+        //ensuring the user enters a number for the price
+        if(isNaN (prodPrice)) {
             res.status(400).json({
-                message: "Something went wrong."
+                message: `prodPrice property must be numeric.`
             }).send();
         }
         else {
+            const result = await db.addItem(prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize);
+
             //store the returned item and print it out for the user
             const item = result[0];
     
@@ -72,43 +74,45 @@ const addItem = async (req, res) => {
 }
 
 const updateItem = async (req, res) => {
-    //making sure the ID is a number
-    if(isNaN(req.params.id)) {
+    //making sure the non-NULLables aren't NULL
+    let id = req.params.id;
+    const {prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize} = req.body;
+
+    if(!id || !prodType || !prodPrice || !prodName) {
         res.status(400).json({
-            message: `Please enter a numeric ID.`
+            message: `ID, prodType, prodPrice and prodName cannot be NULL.`
         }).send();
     }
-    else { 
-            const {prodType, prodPrice, prodName} = req.body;
+    else
+    {
+        //making sure the ID is a number
+        if(isNaN(id) || isNaN(prodPrice)) {
+            res.status(400).json({
+                message: `ID and prodPrice must be numeric.`
+            }).send();
+        }
+        else { 
+            id = parseInt(id);
+            const result = await db.updateItem(prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize, id);
 
-            //making sure prodPrice is a number, too
-            if(isNaN (prodPrice)) {
-                res.status(400).json({
-                    message: `prodPrice property must be numeric.`
+            //if nothing in the DB matches the id
+            if (result === null) {
+                res.status(404).json({
+                    message: `No record found with ID #${id}`
                 }).send();
             }
             else {
-                const id = parseInt(req.params.id);
-                const result = await db.updateItem(prodType, prodPrice, prodName, id);
-
-                //if nothing in the DB matches the id
-                if (result === null) {
-                    res.status(404).json({
-                        message: `No record found with ID #${id}`
-                    }).send();
-                }
-                else {
-                    const item = result[0];
-                    const product = await db.getItemByID(id);
+                const item = result[0];
+                const product = await db.getItemByID(id);
             
-                    res.status(200).json({
-                        message: `Item with ID #${id} has been updated.`,
-                        product
-                    }).send();
-                }
-            }  
-        }
+                res.status(200).json({
+                    message: `Item with ID #${id} has been updated.`,
+                    product
+                }).send();
+            }
+        }  
     }
+}
 
 const deleteItem = async (req, res) => {
     //could probably make this number-checking its own function... hmm...
@@ -120,6 +124,8 @@ const deleteItem = async (req, res) => {
     else {
         //get the ID out of the request body
         const id = parseInt(req.params.id);
+        //I wonder if it's more memory-efficient to read the param twice, or to just store it as a "let" variable?
+        
         const result = await db.deleteItem(id);
 
         //if there was a result, let the user know
