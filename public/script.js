f84a72e1407fcb557679905b20212e673086e29d
jEllerGRC <eller.jared@student.greenriver.edu>
2024-11-14 22:40:28 -0800

    Implemented functionality for reading, creating and deleting data in the "Items" table

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
+//should I declare some global variables up here? And some listeners that exist outside of functions?
+
+//keep the form in a variable for later
+const form = document.querySelector("#new-item");
+
+//this listens for someone to hit the submit button, then overrides the "default submission"
+//behavior and instead calls newItem() down below
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    newItem();
+});
+
 window.onload = () => {
     getData("items");
     getData("orders");
@@ -15,7 +27,8 @@ const getData = async path => {
         }
     };
 
-    //use the fetch API to make a request and get info
+    //GET response status code: 200 (OK); no other codes are programmed for GET
+    //may need to account for server errors or something though...
     const response = await fetch(uri, config);
     const results = await response.json();
     
@@ -23,6 +36,7 @@ const getData = async path => {
     switch (path) {
         //if the path is "items", build rows for the items table.
         case "items":
+            //maybe see about adding a "clear table" function?
             buildItems(results.prods);
             break;
         //if the path is "orders", build rows for the orders table.    
@@ -35,22 +49,25 @@ const getData = async path => {
 //I wonder if I could make this generic and accept either prods OR ords...
 const buildItems = prods => {
     //select the table itself first
-    const table = document.querySelector("#products table");
+    const table = document.querySelector("#products-table");
 
     //for each product in the list of products, populate a new element with its info and append to the table
     for (const thing of prods) {
         //make a new row
         const row = document.createElement("tr");
+        row.className = "itemRow";
 
-        //ID, Name, Category, Price, Quantity in stock... this is gonna be a bit ugly.
+        //ID, Name, Category, Price, Quantity in stock, plus 2 buttons... this is gonna be a bit ugly.
         const id = document.createElement("td");
         const name = document.createElement("td");
         const cat = document.createElement("td");
         const pr = document.createElement("td");
         const quan = document.createElement("td");
+        const edit = document.createElement("button");
+        const del = document.createElement("button");
 
         //put the cells in the row
-        row.append(id, name, cat, pr, quan);
+        row.append(id, name, cat, pr, quan, edit, del);
 
         id.textContent = thing.prodID;
         name.textContent = thing.prodName;
@@ -67,14 +84,206 @@ const buildItems = prods => {
             quan.textContent = thing.stock;
         }
 
-        //currently erroring due to row being NULL somehow...
-        console.log(row);
-        console.log(isNaN(row));
+        edit.innerHTML = "Edit";
+        edit.className = "editItem";
+        del.innerHTML = "Delete";
+        del.className = "delItem";
+
+        //if the buttons are clicked, call their respective functions and pass in the ID
+        
+        //KEEP THESE COMMENTED FOR YOUR OWN GOOD
+        // edit.click = editItem(id.textContent);
+        // del.onclick = deleteItem(id.textContent);
    
-        // table.append(row);
+        table.append(row);
+    }
+    //adding a generic click listener for the table
+    table.addEventListener("click", handleClicks);
+}
+
+//utility function
+const handleClicks = event => {
+    //let's start by grabbing the ID of the product and the table itself
+    const id = event.target.parentNode.children[0].innerHTML;
+    
+    //check the class of the button that was clicked
+    //This is more switch statements than I've written in the past 2 years!
+    switch (event.target.className) {
+        case "editItem" : 
+            editItem(id);
+            break;
+        case "delItem" :
+            deleteItem(id);
+            break;
     }
 }
 
+//working
 const buildOrders = ords => {
     console.log("Orders... dude."); //replace this once you get buildItems functional, repurpose that for this one
+}
+
+//working
+const newItem = async () => {
+
+    //create a formData object and populate it with form data; I read about this from MDN
+    //source: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
+    const formData = new FormData(form);
+
+    //converting from FormData's special content type to plain old JSON, since that's what my application expects
+    const formDataData = {};
+
+    //strange format for the iterator, suppose this helps me get used to inline functions though
+    formData.forEach((key, value) => {
+        formDataData[value] = key; //for some reason the keys and values got jumbled up, so this sets them the "right" way
+    });
+
+    //this feels distinctly kludgey
+    jsonData = JSON.stringify(formDataData);
+
+    //crazy thing here: I originally thought the form itself had to be created by clicking a button on the site
+    //instead of it just being part of the base HTML file
+    const uri = `http://localhost:3030/items`;
+    const config = {
+        method: "post",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonData
+    };
+
+    //we need to keep in mind any errors (like the database service being down)
+    try {
+        //send it off and print out the result of the fetch call
+        const response = await fetch(uri, config);
+        const result = await response.json();
+
+        /*
+        POST status codes:
+        201 (Created)- Item added to DB
+        400 (Bad Request)- Incorrectly-formatted data
+
+        So, if !response.ok, tell the user they did something wrong
+        */
+
+        console.log(result);
+        wipeTable();
+        getData("items");
+    }
+    catch (err) {
+        //something got borked.
+        console.log(err);
+    }
+}
+
+//utility function
+const wipeTable = async () => {
+    //should probably make table a constant at this point...
+    const table = document.querySelector("#products-table");
+
+    //selecting every row that isn't the header
+    const rows = table.querySelectorAll("tr:not(:first-child)");
+    
+    //for each row in the collection, remove it by passing it to an arrow function.
+    rows.forEach(row => row.remove());
+    
+    //getting the table to NOT remove the header row was a headache... I need some coffee after this.
+}
+
+//so, each of these functions corresponds to a given button... feel like I should be outsourcing these to their own file
+const editItem = async id => {
+    //pop up a form and let the user input data... but I gotta validate the data to make sure it's well-formatted, too...
+    
+
+
+    const uri = `http://localhost:3030/items/${id}`;
+    const config = {
+        method: "update",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    /*
+    UPDATE status codes:
+    200 (OK)- Item was updated
+    400 (Bad Request)- Malformed data
+    404 (Not found)- Item not in DB
+    
+    404 shouldn't be a worry (knock on wood), but I have to handle 400...
+    */
+    const response = await fetch(uri, config);
+    const results = await response.json();
+    console.log(results);
+}
+
+//working
+const deleteItem = async (id) => {
+    //get the table first...
+    const table = document.querySelector("#products-table");
+
+    //check for the provided ID in the table rows
+    for (let i = 0; i < table.children.length; i++) {
+        if (table.children[i].tagName === "TR") {
+            //grabbing the current row
+            const row = table.children[i];
+            if (row.children[0].textContent === id) {
+                //gonna ask the user if they're sure they want to delete the item first
+                const confirm = window.confirm(`Are you sure you want to delete ${row.children[1].textContent} from the database?`);
+                if (confirm) {
+                    row.remove();
+                }
+            }
+        }
+    }
+
+    // make a DELETE HTTP request to the database, right?
+    const uri = `http://localhost:3030/items/${id}`;
+    const config = {
+        method: "delete",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    /*
+    DELETE response status codes:
+    200 (OK)- Item was deleted
+    404 (Not Found)- Item not in DB
+    404 shouldn't be an issue here since the user can only click buttons that correspond to existing entries...
+    I hope... better to error-proof just to be safe.
+    */
+   try {
+    const response = await fetch (uri, config);
+
+    //if the response code is something outside the 200 range, throw an error.
+    if(!response.ok) {
+        throw new Error(`An error has occurred. Status: ${response.status}`);
+    }
+    else { //otherwise, print the results to the console.
+        const results = await response.json();
+        console.log(results);
+    }
+   }
+   catch (err) {
+    //print out the error if there is one
+    console.log(err);
+   }
+}
+
+const newOrder = async () => {
+
+}
+
+//waiting on completion of editItem()
+function editOrder() {
+
+}
+
+//waiting on completion of deleteItem()
+function deleteOrder() {
+
 }
