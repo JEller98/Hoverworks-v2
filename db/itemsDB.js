f02e461042f01f49af7663d49e4c3e4b4a3a9cbf
jEllerGRC <eller.jared@student.greenriver.edu>
2024-12-08 18:29:32 -0800

    Altered the implementation of the database connection.

--- db/itemsDB.js
+++ db/itemsDB.js
@@ -1,6 +1,5 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-import chalk from "chalk";
 
 //import the config
 dotenv.config({
@@ -17,54 +16,82 @@ else {
     host = "localhost"; //if there isn't an environment variable provided for the host, then set it to localhost.
 }
 
-//connect to the database
-const connect = await mysql.createConnection({
-    host: HOST,
-    port: DB_PORT,
-    database: DB_DATABASE,
-    user: DB_USER,
-    password: DB_PASSWORD
-});
-//fill in the port here
-console.log(`Connected to MySQL on ${chalk.blue("Port", DB_PORT)}`);
+//creating connections only when necessary to avoid timeouts and crashes
+async function connectFunc() {
+    return await mysql.createConnection({
+        host: host,
+        port: DB_PORT,
+        database: DB_DATABASE,
+        user: DB_USER,
+        password: DB_PASSWORD
+    });
+}
 //methods to interact with the DB down here
 
 //functioning as intended
 async function getItems() {
+    //creating a connection
+    const connect = await connectFunc();
+
     //destructuring, but only grabbing the results; 'properties' is basically useless
     const [results] = await connect.query("SELECT * FROM products");
+
+    //closing connection, then returning the results
+    await connect.end();
+
     return results;
 }
 
 //this is working. probably.
 async function getItemByID(id) {
+    //new connection!
+    const connect = await connectFunc();
+
     //plugging id into the query value
     const [results] = await connect.query("SELECT * FROM products WHERE prodID = ?", [id]);
+
+    //close the connection!
+    await connect.end();
     return results;
 }
 
 //working, I think?
 async function addItem(prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize) {
+
+    //connect to the database...
+    const connect = await connectFunc();
+
     const [results] = await connect.query(
         "INSERT INTO products (prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize) " +
         "VALUES (?, ?, ?, ?, ?, ?, ?)", [prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize]);
 
         //if no rows are affected, cool, it's a new product!
         if (results.affectedRows === 0) {
+            //close the connection
+            await connect.end();
             return null;
         }
         else {
             //otherwise, return what's already in there
             const newProduct = await getItemByID(results.insertId);
+
+            //close the connection
+            await connect.end();
             return newProduct;
         }
 }
 
 async function updateItem (prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize, id) {
+    //connect again
+    const connect = await connectFunc();
+
     const [results] = await connect.query("UPDATE products " +
         "SET prodType = ?, prodPrice = ?, prodName = ?, prodDesc = ?, stock = ?, deckLength = ?, skateSize = ? WHERE prodID = ?",
         [prodType, prodPrice, prodName, prodDesc, stock, deckLength, skateSize, id]);
 
+    //close the connection
+    await connect.end();
+    
     //no affected rows means nothing was updated.
     if (results.affectedRows === 0) {
         return null;
@@ -76,8 +103,14 @@ async function updateItem (prodType, prodPrice, prodName, prodDesc, stock, deckL
 
 //working mostly as intended, I think, possibly, maybe.
 async function deleteItem(id) {
+    //create a connection to the database
+    const connect = await connectFunc();
+
     const [results] = await connect.query("DELETE FROM products WHERE prodID = ?", [id]);
     
+    //close the connection, it's not needed any more.
+    await connect.end();
+
     //beginning to think I should be making this affectedRows check into its own function. Maybe in a future iteration...
     if (results.affectedRows === 0) {
         return null;
--- db/ordersDB.js
+++ db/ordersDB.js
@@ -1,58 +1,81 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-import chalk from "chalk";
 
 //import the config
 dotenv.config({
     path: "./config.env"
 });
 
-const {DB_DATABASE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD} = process.env;
+const {DB_DATABASE, DB_PORT, DB_USER, DB_PASSWORD} = process.env;
 
-//connect to the database
-const connect = await mysql.createConnection({
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_DATABASE,
-    user: DB_USER,
-    password: DB_PASSWORD
-});
-//fill in the port here
-console.log(`Connected to MySQL on ${chalk.magenta("Port", DB_PORT)}`);
+let host;
+if (process.env.DB_HOST) { //if there's an environment variable provided for the host
+    host = process.env.DB_HOST; //assign it
+}
+else {
+    host = "localhost"; //if there isn't an environment variable provided for the host, then set it to localhost.
+}
+
+//creating connections only when necessary to avoid timeouts and crashes
+async function connectFunc() {
+    return await mysql.createConnection({
+        host: host,
+        port: DB_PORT,
+        database: DB_DATABASE,
+        user: DB_USER,
+        password: DB_PASSWORD
+    });
+}
 
 //repurposed methods from itemsDB
 async function getOrders() {
+    const connect = await connectFunc();
+
     //grab everything
     const [results] = await connect.query("SELECT * FROM orders");
+    
+    await connect.end();
     return results;
 }
 
 async function getOrderByID(id) {
+    const connect = await connectFunc();
+
     //plug in the ID
     const [results] = await connect.query("SELECT * FROM orders WHERE orderID = ?", [id]);
+
+    await connect.end();
     return results;
 }
 
 async function addOrder(orderName, orderSubTotal, orderTax, orderPromo, orderTotal, orderStatus) {
+    const connect = await connectFunc();
+
     const [results] = await connect.query(
         "INSERT INTO orders (orderName, orderSubTotal, orderTax, orderPromo, orderTotal, orderDate, orderStatus) " +
         "VALUES (?, ?, ?, ?, ?, NOW(), ?)", [orderName, orderSubTotal, orderTax, orderPromo, orderTotal, orderStatus]);
 
         //no rows affected
         if (results.affectedRows === 0) {
+            await connect.end();
             return null;
         }
         else {
             //return what already exists instead
             const newOrder = await getOrderByID(results.insertId);
+
+            await connect.end();
             return newOrder;
         }
 }
 
 async function updateOrder (orderName, orderSubTotal, orderTax, orderPromo, orderTotal, orderStatus, orderID) {
+    const connect = await connectFunc();
     const [results] = await connect.query("UPDATE orders " +
         "SET orderName = ?, orderSubTotal = ?, orderTax = ?, orderPromo = ?, orderTotal = ?, orderStatus = ? WHERE orderID = ?", 
         [orderName, orderSubTotal, orderTax, orderPromo, orderTotal, orderStatus, orderID]);
+
+    await connect.end();
 
     //no affected rows? no updates
     if (results.affectedRows === 0) {
@@ -65,8 +88,11 @@ async function updateOrder (orderName, orderSubTotal, orderTax, orderPromo, orde
 
 
 async function deleteOrder(id) {
+    const connect = await connectFunc();
     const [results] = await connect.query("DELETE FROM orders WHERE orderID = ?", [id]);
     
+
+    await connect.end();
     //beginning to think I should be making this affectedRows check into its own function. Maybe in a future iteration...
     if (results.affectedRows === 0) {
         return null;
